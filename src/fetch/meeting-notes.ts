b09ed4063c397d405e013a7fb1df2649cacdffd3
Jack Works <jackworks@protonmail.com>
2022-07-30 15:15:37 +0800

    optimize how to find meeting notes

---
+++ src/fetch/meeting-notes.ts
@@ -0,0 +1,45 @@
+import { readFile, readdir } from 'node:fs/promises';
+
+const contents = new Map<string, string>();
+export async function readAllMeetingNotes() {
+  if (contents.size) return contents;
+
+  const result = await readdir(`notes/meetings`);
+
+  const fileToRead: string[] = [];
+  for (const date of result) {
+    // notes before 2016 does not have a [proposal](link) field.
+    if (parseFloat(date) < 2016) continue;
+    for (const file of await readdir(`notes/meetings/${date}`)) {
+      if (!file.endsWith('.md')) continue;
+
+      fileToRead.push(`notes/meetings/${date}/${file}`);
+    }
+  }
+  await Promise.all(
+    fileToRead.map(async (file) => {
+      contents.set(file, await readFile(file, 'utf-8'));
+    }),
+  );
+  return contents;
+}
+
+export async function findURLPresentInMeetingNotes(url: string): Promise<[string, Date][]> {
+  const notes = await readAllMeetingNotes();
+  return Array.from(notes)
+    .filter((x) => x[1].includes(url))
+    .map((x) => x[0])
+    .map(pathToURL_DatePair);
+}
+
+function pathToURL_DatePair(path: string): [string, Date] {
+  // notes/meetings/2022/01/jan-12.md
+  console.assert(path.startsWith('notes/meetings/'));
+  const [_, __, yearMonth, monthDay] = path.split('/');
+  const [year, month] = yearMonth.split('-');
+  const [month2, dayInMonth] = monthDay.slice(0, -3).split('-');
+
+  console.log(year, month, dayInMonth);
+  const date = new Date(`${year}-${month}-${dayInMonth}`);
+  return [`https://github.com/tc39/notes/blob/main/meetings/${yearMonth}/${monthDay}.md`, date];
+}
--- src/fetch/proposal-markdown.ts
+++ src/fetch/proposal-markdown.ts
@@ -3,6 +3,7 @@ import _ from 'lodash-es';
 import MarkdownIt from 'markdown-it';
 import { parseHTML } from './parse-table.js';
 import { BundleProposals, IndividualProposal } from '../types/bundle.js';
+import { findURLPresentInMeetingNotes } from './meeting-notes.js';
 
 const markdown = new MarkdownIt();
 
@@ -26,19 +27,21 @@ export async function readAllProposals() {
   ];
   const records: BundleProposals = [];
   for (const { tags, stages, path } of jobs) {
-    records.push(...readProposals(tags, stages, await readFile(`proposals/${path}.md`, 'utf-8')));
+    for await (const item of readProposals(tags, stages, await readFile(`proposals/${path}.md`, 'utf-8'))) {
+      records.push(item);
+    }
   }
   return records;
 }
 
-function* readProposals(tags: IndividualProposal['tags'], stages: number[], content: string): Generator<BundleProposals[0]> {
+async function* readProposals(tags: IndividualProposal['tags'], stages: number[], content: string): AsyncGenerator<BundleProposals[0]> {
   let i = 0;
   for (const table of parseHTML(markdown.render(content), renameHeader)) {
     console.log(`Parsing ${tags[0]} Stage ${stages[i]}`);
     for (const row of table) {
       const test = _.values(row.tests?.links)[0]?.trim();
       const meeting = _.values(row.meeting?.links)[0]?.trim();
-      yield {
+      const proposal: BundleProposals[0] = {
         tags: Array.from(tags) as any,
         stage: stages[i],
         name: row.name?.text?.trim()!,
@@ -57,6 +60,20 @@ function* readProposals(tags: IndividualProposal['tags'], stages: number[], cont
         rationale: row.rationale?.text?.trim(),
         edition: row.edition ? +row.edition.text : undefined,
       };
+      if (proposal.url) {
+        const data = await findURLPresentInMeetingNotes(proposal.url);
+        if (data.length) {
+          if (!proposal.notes) proposal.notes = [] as any;
+          for (const [url, date] of data) {
+            proposal.notes!.push({
+              date: date.toISOString(),
+              url,
+            });
+          }
+        }
+      }
+      proposal.notes?.sort((a, b) => Number(b.date) - Number(a.date));
+      yield proposal;
     }
     i++;
   }
